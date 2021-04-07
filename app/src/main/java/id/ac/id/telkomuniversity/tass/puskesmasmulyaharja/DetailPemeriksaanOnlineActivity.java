78ea42da502b31a722dcdc0be01ab5f093141548
aldinaufal21 <ry.prg75@gmail.com>
2021-04-07 19:12:12 +0700

    app v1 (add layout)

---
+++ app/src/main/java/id/ac/id/telkomuniversity/tass/puskesmasmulyaharja/DetailPemeriksaanOnlineActivity.java
@@ -0,0 +1,14 @@
+package id.ac.id.telkomuniversity.tass.puskesmasmulyaharja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailPemeriksaanOnlineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_pemeriksaan_online);
+    }
+}
---
+++ app/src/main/java/id/ac/id/telkomuniversity/tass/puskesmasmulyaharja/KtpActivity.java
@@ -0,0 +1,14 @@
+package id.ac.id.telkomuniversity.tass.puskesmasmulyaharja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KtpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ktp);
+    }
+}
